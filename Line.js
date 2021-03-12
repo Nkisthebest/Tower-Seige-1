705b343f3546ad8af995fd3b668c6e1746284bef
Nkisthebest <77501459+Nkisthebest@users.noreply.github.com>
2021-03-12 16:38:05 +0530

    Add files via upload

---
+++ Line.js
@@ -0,0 +1,25 @@
+class Line{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.002,
+            length: 10,
+        }
+        this.pointB = pointB
+        this.Line= Constraint.create(options);
+        World.add(world, this.Line);
+    }
+    fly(){
+        this.Line.bodyA=null
+    }
+      display(){
+          if(this.Line.bodyA){
+            var pointA = this.Line.bodyA.position;
+            var pointB = this.pointB;
+            stroke("blue")
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+          }
+    } 
+}
